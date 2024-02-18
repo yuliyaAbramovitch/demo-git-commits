611f19d84db10bef9785bcdd3d25250de46d3184
Yuliya Abramovitch <yuabramovitch@gmail.com>
2024-02-18 21:23:19 +0300

    Add Sout

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,7 +1,8 @@
-public Class HelloWorld {
-	public static void main (Strings[] args){
-		User user = new User();
-		user.name = "Robin";
-		user.password = "qwerty";
-	}
+public class HelloWorld {
+    public static void main(String[] args) {
+        User user = new User();
+        user.name = "Robin";
+        user.password = "qwerty";
+		System.out.println("Hello world!");
+    }
 }
